b562bb57058ba491bfe3cc62678c6a35acf67181
Kacper <student48955@dsw.edu.pl>
2023-06-15 11:35:08 +0200

    dodano --date

---
+++ zad1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ "$1" == "--date" ]; then
+    date
+:<<C
+elif [ "$1" == "--logs" ]; then
+    if [ -z "$2" ]; then
+        num_logs=100
+    else
+        num_logs=$2
+    fi
+
+    for ((i=1; i<=$num_logs; i++)); do
+        nazwa="log$i.txt"
+        echo "Nazwa pliku: $nazwa" >> $nazwa
+        echo "Nazwa skryptu: $0" >> $nazwa
+        echo "Data: $(date)" >> $nazwa
+    done
+:<<COMMENT
+elif [ "$1" == "--help" ]; then
+    echo "Dostępne opcje:"
+    echo "  --date    - wyświetla dzisiejszą datę"
+    echo "  --logs    - tworzy automatycznie 100 plików logx.txt"
+    echo "  --logs N  - tworzy automatycznie N plików logx.txt"
+
+else
+    echo "Nieznana opcja. Użyj '$0 --help' aby wyświetlić dostępne opcje."
+fi
+C
